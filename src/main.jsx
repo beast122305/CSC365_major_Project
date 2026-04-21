cf988e159441b9ba116e0eab65b8a542aa67144f
Shawn Kalisiak <157556706+beast122305@users.noreply.github.com>
2026-04-21 18:21:15 +0000

    moved project to root for page deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './index.css'
 import App from './App'
-import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 )
